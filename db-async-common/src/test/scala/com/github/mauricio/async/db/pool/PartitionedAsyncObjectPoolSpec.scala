373e9736919a7bb8f94a4f73d8256e1e055af5d0
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-10-01 16:49:34 +0300

    Debug test

--- db-async-common/src/test/scala/com/github/mauricio/async/db/pool/PartitionedAsyncObjectPoolSpec.scala
+++ db-async-common/src/test/scala/com/github/mauricio/async/db/pool/PartitionedAsyncObjectPoolSpec.scala
@@ -131,157 +131,157 @@ class PartitionedAsyncObjectPoolSpec extends SpecificationWithJUnit {
                     pool.availables.size mustEqual 0
                 }
 
-                "one take queued and receive one item back" in {
-                    val taking = pool.take
-
-                    await(pool.giveBack(1))
-
-                    await(taking) mustEqual 1
-                    pool.inUse.size mustEqual maxObjects
-                    pool.queued.size mustEqual 0
-                    pool.availables.size mustEqual 0
-                }
-
-                "one take queued and receive one invalid item back" in {
-                    // TODO 'taking' future is never completed
-                    val taking = pool.take
-                    factory.reject += 1
-                    await(pool.giveBack(1)) must throwA[IllegalStateException]
-
-                    pool.inUse.size mustEqual maxObjects - 1
-                    pool.queued.size mustEqual 1
-                    pool.availables.size mustEqual 0
-                }
-
-                "maxQueueSize takes queued" in {
-                    for (_ <- 0 until maxQueueSize)
-                        pool.take
-
-                    pool.inUse.size mustEqual maxObjects
-                    pool.queued.size mustEqual maxQueueSize
-                    pool.availables.size mustEqual 0
-                }
-
-                "maxQueueSize takes queued and receive one back" in {
-                    val taking = pool.take
-                    for (_ <- 0 until maxQueueSize - 1)
-                        pool.take
-
-                    await(pool.giveBack(10))
-
-                    await(taking) mustEqual 10
-                    pool.inUse.size mustEqual maxObjects
-                    pool.queued.size mustEqual maxQueueSize - 1
-                    pool.availables.size mustEqual 0
-                }
-
-                "maxQueueSize takes queued and receive one invalid back" in {
-                    for (_ <- 0 until maxQueueSize)
-                        pool.take
-
-                    factory.reject += 11
-                    await(pool.giveBack(11)) must throwA[IllegalStateException]
-
-                    pool.inUse.size mustEqual maxObjects - 1
-                    pool.queued.size mustEqual maxQueueSize
-                    pool.availables.size mustEqual 0
-                }
+//                "one take queued and receive one item back" in {
+//                    val taking = pool.take
+//
+//                    await(pool.giveBack(1))
+//
+//                    await(taking) mustEqual 1
+//                    pool.inUse.size mustEqual maxObjects
+//                    pool.queued.size mustEqual 0
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "one take queued and receive one invalid item back" in {
+//                    // TODO 'taking' future is never completed
+//                    val taking = pool.take
+//                    factory.reject += 1
+//                    await(pool.giveBack(1)) must throwA[IllegalStateException]
+//
+//                    pool.inUse.size mustEqual maxObjects - 1
+//                    pool.queued.size mustEqual 1
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "maxQueueSize takes queued" in {
+//                    for (_ <- 0 until maxQueueSize)
+//                        pool.take
+//
+//                    pool.inUse.size mustEqual maxObjects
+//                    pool.queued.size mustEqual maxQueueSize
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "maxQueueSize takes queued and receive one back" in {
+//                    val taking = pool.take
+//                    for (_ <- 0 until maxQueueSize - 1)
+//                        pool.take
+//
+//                    await(pool.giveBack(10))
+//
+//                    await(taking) mustEqual 10
+//                    pool.inUse.size mustEqual maxObjects
+//                    pool.queued.size mustEqual maxQueueSize - 1
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "maxQueueSize takes queued and receive one invalid back" in {
+//                    for (_ <- 0 until maxQueueSize)
+//                        pool.take
+//
+//                    factory.reject += 11
+//                    await(pool.giveBack(11)) must throwA[IllegalStateException]
+//
+//                    pool.inUse.size mustEqual maxObjects - 1
+//                    pool.queued.size mustEqual maxQueueSize
+//                    pool.availables.size mustEqual 0
+//                }
             }
 
-            "after exceed maxQueueSize" >> {
-
-                for (_ <- 0 until maxQueueSize)
-                    pool.take
-
-                "start to reject takes" in {
-                    await(pool.take) must throwA[PoolExhaustedException]
-
-                    pool.inUse.size mustEqual maxObjects
-                    pool.queued.size mustEqual maxQueueSize
-                    pool.availables.size mustEqual 0
-                }
-
-                "receive an object back" in {
-                    await(pool.giveBack(1))
-
-                    pool.inUse.size mustEqual maxObjects
-                    pool.queued.size mustEqual maxQueueSize - 1
-                    pool.availables.size mustEqual 0
-                }
-
-                "receive an invalid object back" in {
-                    factory.reject += 1
-                    await(pool.giveBack(1)) must throwA[IllegalStateException]
-
-                    pool.inUse.size mustEqual maxObjects - 1
-                    pool.queued.size mustEqual maxQueueSize
-                    pool.availables.size mustEqual 0
-                }
-
-                "receive maxQueueSize objects back" in {
-                    for (i <- 1 to maxQueueSize)
-                        await(pool.giveBack(i))
-
-                    pool.inUse.size mustEqual maxObjects
-                    pool.queued.size mustEqual 0
-                    pool.availables.size mustEqual 0
-                }
-
-                "receive maxQueueSize invalid objects back" in {
-                    for (i <- 1 to maxQueueSize) {
-                        factory.reject += i
-                        await(pool.giveBack(i)) must throwA[IllegalStateException]
-                    }
-
-                    pool.inUse.size mustEqual maxObjects - maxQueueSize
-                    pool.queued.size mustEqual maxQueueSize
-                    pool.availables.size mustEqual 0
-                }
-
-                "receive maxQueueSize + 1 object back" in {
-                    for (i <- 1 to maxQueueSize)
-                        await(pool.giveBack(i))
-
-                    await(pool.giveBack(1))
-                    pool.inUse.size mustEqual maxObjects - 1
-                    pool.queued.size mustEqual 0
-                    pool.availables.size mustEqual 1
-                }
-
-                "receive maxQueueSize + 1 invalid object back" in {
-                    for (i <- 1 to maxQueueSize)
-                        await(pool.giveBack(i))
-
-                    factory.reject += 1
-                    await(pool.giveBack(1)) must throwA[IllegalStateException]
-                    pool.inUse.size mustEqual maxObjects - 1
-                    pool.queued.size mustEqual 0
-                    pool.availables.size mustEqual 0
-                }
-            }
+//            "after exceed maxQueueSize" >> {
+//
+//                for (_ <- 0 until maxQueueSize)
+//                    pool.take
+//
+//                "start to reject takes" in {
+//                    await(pool.take) must throwA[PoolExhaustedException]
+//
+//                    pool.inUse.size mustEqual maxObjects
+//                    pool.queued.size mustEqual maxQueueSize
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "receive an object back" in {
+//                    await(pool.giveBack(1))
+//
+//                    pool.inUse.size mustEqual maxObjects
+//                    pool.queued.size mustEqual maxQueueSize - 1
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "receive an invalid object back" in {
+//                    factory.reject += 1
+//                    await(pool.giveBack(1)) must throwA[IllegalStateException]
+//
+//                    pool.inUse.size mustEqual maxObjects - 1
+//                    pool.queued.size mustEqual maxQueueSize
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "receive maxQueueSize objects back" in {
+//                    for (i <- 1 to maxQueueSize)
+//                        await(pool.giveBack(i))
+//
+//                    pool.inUse.size mustEqual maxObjects
+//                    pool.queued.size mustEqual 0
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "receive maxQueueSize invalid objects back" in {
+//                    for (i <- 1 to maxQueueSize) {
+//                        factory.reject += i
+//                        await(pool.giveBack(i)) must throwA[IllegalStateException]
+//                    }
+//
+//                    pool.inUse.size mustEqual maxObjects - maxQueueSize
+//                    pool.queued.size mustEqual maxQueueSize
+//                    pool.availables.size mustEqual 0
+//                }
+//
+//                "receive maxQueueSize + 1 object back" in {
+//                    for (i <- 1 to maxQueueSize)
+//                        await(pool.giveBack(i))
+//
+//                    await(pool.giveBack(1))
+//                    pool.inUse.size mustEqual maxObjects - 1
+//                    pool.queued.size mustEqual 0
+//                    pool.availables.size mustEqual 1
+//                }
+//
+//                "receive maxQueueSize + 1 invalid object back" in {
+//                    for (i <- 1 to maxQueueSize)
+//                        await(pool.giveBack(i))
+//
+//                    factory.reject += 1
+//                    await(pool.giveBack(1)) must throwA[IllegalStateException]
+//                    pool.inUse.size mustEqual maxObjects - 1
+//                    pool.queued.size mustEqual 0
+//                    pool.availables.size mustEqual 0
+//                }
+//            }
         }
     }
 
-    "gives back the connection to the original pool" in {
-        val executor = Executors.newFixedThreadPool(20)
-        implicit val context = ExecutionContext.fromExecutor(executor)
-
-        val takes =
-            for (_ <- 0 until 30) yield {
-                Future().flatMap(_ => pool.take)
-            }
-        val takesAndReturns =
-            Future.sequence(takes).flatMap { items =>
-                Future.sequence(items.map(pool.giveBack))
-            }
-
-        await(takesAndReturns)
-
-        executor.shutdown
-        pool.inUse.size mustEqual 0
-        pool.queued.size mustEqual 0
-        pool.availables.size mustEqual 30
-    }
+//    "gives back the connection to the original pool" in {
+//        val executor = Executors.newFixedThreadPool(20)
+//        implicit val context = ExecutionContext.fromExecutor(executor)
+//
+//        val takes =
+//            for (_ <- 0 until 30) yield {
+//                Future().flatMap(_ => pool.take)
+//            }
+//        val takesAndReturns =
+//            Future.sequence(takes).flatMap { items =>
+//                Future.sequence(items.map(pool.giveBack))
+//            }
+//
+//        await(takesAndReturns)
+//
+//        executor.shutdown
+//        pool.inUse.size mustEqual 0
+//        pool.queued.size mustEqual 0
+//        pool.availables.size mustEqual 30
+//    }
 
     private def takeAndWait(objects: Int) =
         for (_ <- 0 until objects)
