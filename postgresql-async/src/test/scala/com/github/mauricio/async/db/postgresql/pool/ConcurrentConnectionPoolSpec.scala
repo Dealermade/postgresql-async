49e3de9f66f18fbb0183d0982be6ab0638f9866c
Oleksandr Bezhan <oleksandr.bezhan@gmail.com>
2018-10-03 08:58:04 +0300

    Added concurrent test for connection pool.

--- postgresql-async/src/test/scala/com/github/mauricio/async/db/postgresql/pool/ConcurrentConnectionPoolSpec.scala
+++ postgresql-async/src/test/scala/com/github/mauricio/async/db/postgresql/pool/ConcurrentConnectionPoolSpec.scala
@@ -1,29 +1,43 @@
 package com.github.mauricio.async.db.postgresql.pool
 
+import com.github.mauricio.async.db.QueryResult
 import com.github.mauricio.async.db.pool.{ConnectionPool, PoolConfiguration}
 import com.github.mauricio.async.db.postgresql.DatabaseTestHelper
 import org.specs2.mutable.Specification
-import scala.concurrent.ExecutionContext.Implicits.global
+import org.specs2.specification.Scope
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class ConcurrentConnectionPoolSpec extends Specification with DatabaseTestHelper {
-  "pool" should {
-    "execute simple queries in parallel" in {
-      val pool = new ConnectionPool(new PostgreSQLConnectionFactory(defaultConfiguration), PoolConfiguration.Default)
-      val queries = for (i <- 0 until 10) yield {
-        Future {
-          i -> pool.sendQuery(s"SELECT $i")
-        }
+
+  trait Context extends Scope {
+    val poolConfig: PoolConfiguration = PoolConfiguration.Default
+    val pool = new ConnectionPool(new PostgreSQLConnectionFactory(defaultConfiguration), poolConfig)
+  }
+
+  "connection pool" should {
+    "execute simple queries in parallel" in new Context {
+      val queries = for (i <- 0 until poolConfig.maxObjects) yield {
+        Future(i -> pool.sendQuery(s"SELECT $i"))
       }
 
-      await(Future.sequence(queries)).foreach {
-        case (i, resultFuture) =>
-          val rows = await(resultFuture).rows
-          rows must beSome
-          rows.get(0)(0) mustEqual i
+      assertResultsMatchIndexes(queries)
+    }
+    "execute queued queries in parallel" in new Context {
+      val queries = for (i <- 0 until poolConfig.maxObjects + poolConfig.maxQueueSize) yield {
+        Future(i -> pool.sendQuery(s"SELECT $i"))
       }
-      success
+
+      assertResultsMatchIndexes(queries)
+    }
+  }
+
+  private def assertResultsMatchIndexes(queries: Seq[Future[(Int, Future[QueryResult])]]): Unit = {
+    await(Future.sequence(queries)).foreach {
+      case (i, resultFuture) =>
+        val rows = await(resultFuture).rows
+        rows.map(x => x.apply(0)(0)) === Some(i)
     }
   }
 }
